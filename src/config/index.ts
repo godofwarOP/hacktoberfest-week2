b73beca5769da8d6b322808639cac15fe98baaf8
godofwarOP <pratik11299gahane@gmail.com>
2022-10-16 15:57:59 +0530

    added guild and application id as env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ export class Config {
   fetch = fetch;
   public token: string = "";
   public trackerggApiKey: string = "";
+  public guildId: string = "";
+  public applicationId: string = "";
 
   public constructor() {
     this.init();
@@ -19,8 +21,22 @@ export class Config {
         "Trackergg API key is missing, did you forget to set it as env variable?"
       );
 
+    if (!process.env.APPLICATION_ID)
+      throw new Error(
+        "Application Id is missing, did you forget to set it as env variable?"
+      );
+
+    if (process.env.NODE_ENV === "development") {
+      if (!process.env.GUILD_ID)
+        throw new Error(
+          "Guild Id is missing, did you forget to set it as env variable?"
+        );
+      this.guildId = process.env.GUILD_ID;
+    }
+
     this.token = process.env.BOT_TOKEN;
     this.trackerggApiKey = process.env.TRACKERGG_API_KEY;
+    this.applicationId = process.env.APPLICATION_ID;
   }
 
   init() {
